d6f42c4934a707afcc73bebf0f7398a6f79f58ea
lyylyylyylyy <lyyzju@outlook.com>
2020-03-02 21:12:22 +0800

    fix bug

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import {HashRouter, Switch, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
 
 import store from './redux/store'
-import Rigister from './containers/register/register'
+import Register from './containers/register/register'
 import Login from './containers/login/login'
 import Main from './containers/main/main'
 
@@ -19,7 +19,7 @@ ReactDOM.render((
     <Provider store={store}>
     <HashRouter>
     <Switch>
-    <Route path="/register" component={Rigister}/>
+    <Route path="/register" component={Register}/>
 <Route path="/login" component={Login}/>
 <Route component={Main}></Route> 默认路由
     </Switch>
